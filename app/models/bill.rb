86eee5bbd530f8ea04f82b7211785d96edf41ffb
Corey Schneider <coreyschneider2@gmail.com>
2022-09-02 20:28:56 -0400

    validate user1 and user2 to make sure they don't equal each other

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -2,5 +2,12 @@ class Bill < ApplicationRecord
   belongs_to :user
 
   # Required params
-  validates_presence_of :title, :amount, :paid, :transaction_date
+  validates_presence_of :title, :amount, :transaction_date
+  validate :check_users
+
+  protected
+
+  def check_users
+    errors.add(:user_id, 'This user cannot owe/pay themselves') unless user_id != person2_id
+  end
 end
