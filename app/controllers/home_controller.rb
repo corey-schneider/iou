8d35222ad0dd9b015bfd0842cfc376a1eac15645
Corey Schneider <coreyschneider2@gmail.com>
2022-09-05 11:20:27 -0400

    sort bills by transaction date descending, create grouped bills arr

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
     @bills = Bill.all
-    @sorted_bills = Bill.order(:transaction_date)
+    @sorted_bills = Bill.order(transaction_date: :desc)
+    @grouped_bills = Bill.select(:transaction_date, 'array_agg(id)')
+                         .group(:transaction_date)
+                         .order(transaction_date: :desc)
+                         .pluck(:transaction_date, 'array_agg(id)')
+    # @grouped_bills_hash = Hash[*@grouped_bills.flatten(1)]
   end
 end
