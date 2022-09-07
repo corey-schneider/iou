52779e812c99f6802f0e0c845db663186b31dc6d
Corey Schneider <coreyschneider2@gmail.com>
2022-09-07 19:52:49 -0400

    add scope to bill for month

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,7 +3,18 @@ class Bill < ApplicationRecord
 
   # Required params
   validates_presence_of :title, :amount, :transaction_date
+
   validate :check_users
+
+  scope :from_this_month, -> { where(transaction_date: Time.now.beginning_of_month..Time.now.end_of_month) }
+  scope :from_month, ->(date) { where(transaction_date: date.beginning_of_month..date.end_of_month) }
+  scope :from_month_grouped, lambda { |date|
+    from_month(date)
+      .select(:transaction_date, 'array_agg(id)')
+      .group(:transaction_date)
+      .order(transaction_date: :desc)
+      .pluck(:transaction_date, 'array_agg(id)')
+  }
 
   protected
 
