fdbe3d6df81e97deaabe0ba9515aa83ac781cc91
Corey Schneider <coreyschneider2@gmail.com>
2022-12-09 21:06:16 -0500

    format bills on user profile page

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -6,6 +6,8 @@ class Bill < ApplicationRecord
 
   validate :check_users
 
+  default_scope { order(transaction_date: :desc) }
+
   scope :from_this_month, -> { where(transaction_date: Time.now.beginning_of_month..Time.now.end_of_month) }
   scope :from_month, ->(date) { where(transaction_date: date.beginning_of_month..date.end_of_month) }
   scope :from_month_grouped, lambda { |date|
@@ -15,16 +17,21 @@ class Bill < ApplicationRecord
       .order(transaction_date: :desc)
       .pluck(:transaction_date, 'array_agg(id)')
   }
+
   scope :all_grouped, lambda {
     select(:transaction_date, 'array_agg(id)')
       .group(:transaction_date)
       .order(transaction_date: :desc)
       .pluck(:transaction_date, 'array_agg(id)')
   }
-  scope :owed, -> { where(paid: false) }
+
+  scope :owes, -> { where(paid: false) }
+  scope :owed_to, ->(user) { where(person2_id: user.id, paid: false) }
+
   scope :paid, -> { where(paid: true) }
-  scope :amount_owed, -> { owed.sum('bills.amount') }
-  scope :amount_paid, -> { paid.sum('bills.amount') }
+  scope :paid_amount, -> { paid.sum('bills.amount') }
+
+  scope :owes_amount, -> { owes.sum('bills.amount') }
 
   protected
 
